f88e74cde0fb5c85dfded81b956ba1993ac65fda
anastasia pokormiak <a07675109@gmail.com>
2026-01-19 14:48:42 +0200

    additional test commit

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
 print ("anastasia pokormiak, cs-2, group 2 (assigned)")
 print ("group 3 (in fact)")
 print ("let it be an additional visible commit in my repository")
+print ("one more commit just to make sure")
